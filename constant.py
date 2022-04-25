d5d0535e38932ca3c7d6b917951bec10381121c3
Bill Wilson <BillWilson@gmail.com@users.noreply.github.com>
2022-04-25 10:49:32 -0500

    Adding a constant file for easier updates and readability.  Adding a coin command that flips a coin

---
+++ constant.py
@@ -0,0 +1,15 @@
+DICE_NUMBER_LIST = [4,6,8,10,12,20,100]
+REG_DICE_MAX = 6
+COIN_MAX = 2
+NUM_OF_REG_DICE_VARIATIONS = 2
+NUM_ROLLS_CRAPS = 2
+NUM_ROLLS_CEELO = 3
+MAX_NUMBER_ROLLS = 21
+
+BLACK_DICE_FOLDER_PATH = "pictures/black_dice/"
+WHITE_DICE_FOLDER_PATH = "pictures/white_dice/"
+SAD_DICE_KING_FILE_PATH = 'pictures/dice_king/sad_dice_king.png'
+TEMP_FILE_PATH = 'pictures/temp/combined.png'
+COIN_FOLDER_PATH = 'pictures/coin/'
+
+HELP_ME_STRING = "I'm King Dice, proprietor of this server. Your one and only stop for all dice-related needs. Here is how to use me, and good luck friend!\n\n!dice – roll a regular 6-sided die \n!craps – rolls 2 6-sided die\n!ceelo – rolls 3 6-sided die\n!20 – rolls a d20 die, also works for 12/10/8/6/4\n!100 – rolls 2d10, one for the first digit and one for the second (000 is 100)\n!coin or !coinflip - flips a coin\nAdding a number after a command will perform that command X amount of times (ex: !20 4). Max 20 times.\n\nAnd don’t even think about doing a number after the exclamation mark that isn’t listed above this.. "
--- main.py
+++ main.py
@@ -2,25 +2,13 @@
 import os
 import random
 from PIL import Image
-from os.path import exists
+import constant
 
 import discord
 from dotenv import load_dotenv
 
-dice_number_list = [4,6,8,10,12,20,100]
-dice_command_list = ["dice","craps","cee-lo","ceelo"]
-reg_dice_max = 6
-num_of_reg_dice_variations = 2
-num_rolls_craps = 2
-num_rolls_ceelo = 3
-max_number_rolls = 21
-
-sad_dice_king_file_path = 'pictures/dice_king/sad_dice_king.png'
-temp_file_path = 'pictures/temp/combined.png'
-
-help_me_string = "I'm King Dice, proprietor of this server. Your one and only stop for all dice-related needs. Here is how to use me, and good luck friend!\n\n!dice – roll a regular 6-sided die \n!craps – rolls 2 6-sided die\n!ceelo – rolls 3 6-sided die\n!20 – rolls a d20 die, also works for 12/10/8/6/4\n!100 – rolls 2d10, one for the first digit and one for the second (000 is 100)\n\nAnd don’t even think about doing a number after the exclamation mark that isn’t listed above this.. "
-
 async def roll_dice(message,folder_name,dice_max_size,num_of_dice):
+    
     if dice_max_size == 100:
         dice_roll = random.randrange(1,dice_max_size + 1)
         dice_roll_tens = dice_roll // 10
@@ -32,9 +20,9 @@ async def roll_dice(message,folder_name,dice_max_size,num_of_dice):
         image_combined = Image.new('RGBA', (image_1.width + image_2.width, image_1.height))
         image_combined.paste(image_1, (0, 0))
         image_combined.paste(image_2, (image_1.width, 0))
-        image_combined.save(temp_file_path)
-        await message.channel.send(file=discord.File(temp_file_path))
-        os.remove(temp_file_path)
+        image_combined.save(constant.TEMP_FILE_PATH)
+        await message.channel.send(file=discord.File(constant.TEMP_FILE_PATH))
+        os.remove(constant.TEMP_FILE_PATH)
 
     elif num_of_dice > 1:
         images = []
@@ -55,9 +43,9 @@ async def roll_dice(message,folder_name,dice_max_size,num_of_dice):
             image_combined.paste(image,(width,0))
             width = width + image.width
 
-        image_combined.save(temp_file_path)
-        await message.channel.send(file=discord.File(temp_file_path))
-        os.remove(temp_file_path)
+        image_combined.save(constant.TEMP_FILE_PATH)
+        await message.channel.send(file=discord.File(constant.TEMP_FILE_PATH))
+        os.remove(constant.TEMP_FILE_PATH)
 
     else:
         dice_roll = random.randrange(1,dice_max_size  + 1)
@@ -87,7 +75,7 @@ async def on_message(message):
     if message.content.startswith("!"):
         user_message = message.content.replace("!","")
         user_command = user_message.split(' ')
-        if user_command[0].isdigit() and int(user_command[0]) in dice_number_list:
+        if user_command[0].isdigit() and int(user_command[0]) in constant.DICE_NUMBER_LIST:
             dice_size = int(user_command[0])
             if dice_size == 100:
                 dice_folder_name = "pictures/d" + str(dice_size) + "/"
@@ -99,32 +87,42 @@ async def on_message(message):
                 if len(user_command) > 1:
                     if user_command[1].isdigit():
                         num_rolls = int(user_command[1])
-                if num_rolls < max_number_rolls:  
+                if num_rolls < constant.MAX_NUMBER_ROLLS:  
                     await roll_dice(message,dice_folder_name,dice_size,num_rolls)
                 else:
-                    await message.channel.send(file=discord.File(sad_dice_king_file_path))
+                    await message.channel.send(file=discord.File(constant.SAD_DICE_KING_FILE_PATH))
         elif user_command[0].isdigit():
-            await message.channel.send(file=discord.File(sad_dice_king_file_path))
+            await message.channel.send(file=discord.File(constant.SAD_DICE_KING_FILE_PATH))
         else:
-            dice_color = random.randrange(1,num_of_reg_dice_variations + 1)
+            dice_color = random.randrange(1,constant.NUM_OF_REG_DICE_VARIATIONS + 1)
             if dice_color == 1:
-                dice_folder_name = "pictures/black_dice/"
+                dice_folder_name = constant.BLACK_DICE_FOLDER_PATH
             else:
-                dice_folder_name = "pictures/white_dice/"
+                dice_folder_name = constant.WHITE_DICE_FOLDER_PATH
 
             if user_command[0].lower() == "dice":
                 num_rolls = 1
                 if len(user_command) > 1:
                     if user_command[1].isdigit():
                         num_rolls = int(user_command[1])
-                if num_rolls < max_number_rolls:   
-                    await roll_dice(message,dice_folder_name,reg_dice_max,num_rolls)
+                if num_rolls < constant.MAX_NUMBER_ROLLS:   
+                    await roll_dice(message,dice_folder_name,constant.REG_DICE_MAX,num_rolls)
                 else:
-                    await message.channel.send(file=discord.File(sad_dice_king_file_path))
+                    await message.channel.send(file=discord.File(constant.SAD_DICE_KING_FILE_PATH))
             elif user_command[0].lower() == "craps":
-                await roll_dice(message,dice_folder_name,reg_dice_max,num_rolls_craps)
+                await roll_dice(message,dice_folder_name,constant.REG_DICE_MAX,constant.NUM_ROLLS_CRAPS)
             elif user_command[0].lower() == "cee-lo" or user_command[0] == "ceelo":
-                await roll_dice(message,dice_folder_name,reg_dice_max,num_rolls_ceelo)
+                await roll_dice(message,dice_folder_name,constant.REG_DICE_MAX,constant.NUM_ROLLS_CEELO)
+            elif user_command[0].lower() == "coin" or user_command[0] == "coinflip":
+                num_rolls = 1
+                if len(user_command) > 1:
+                    if user_command[1].isdigit():
+                        num_rolls = int(user_command[1])
+                if num_rolls < constant.MAX_NUMBER_ROLLS:   
+                    await roll_dice(message,constant.COIN_FOLDER_PATH,constant.COIN_MAX,num_rolls)
+                else:
+                    await message.channel.send(file=discord.File(constant.SAD_DICE_KING_FILE_PATH))
             elif user_command[0].lower() == "dicehelp":
-                await message.channel.send(help_me_string)
+                await message.channel.send(constant.HELP_ME_STRING)
+
 client.run(TOKEN)
